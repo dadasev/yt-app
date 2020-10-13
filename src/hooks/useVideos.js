894c1038c65e0af5c382c85941351f44d0ff593f
MacBook Pro i7 <MacBook_Pro_i7@Andriaus-Mac.local>
2020-10-13 15:49:32 +0300

    ready for deploy

--- src/hooks/useVideos.js
+++ src/hooks/useVideos.js
@@ -1,6 +1,5 @@
 import {useState, useEffect} from 'react';
 import youtube from '../apis/youtube';
-import React from 'react';
 
 const useVideos = (defaultSearchTerm) => {
     const [videos, setVideos] = useState([]);
